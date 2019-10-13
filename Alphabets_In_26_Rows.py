c993fbc0417d498992e36487b00a30043deb394e
PSI KID Mercy <30193989+PSI-KID-Mercy@users.noreply.github.com>
2019-10-13 17:31:22 +0530

    Create Alphabets_In_26_Rows.py
    In this Program, I represent all the alphabets in 26 rows.

---
+++ Alphabets_In_26_Rows.py
@@ -0,0 +1,19 @@
+/** "Nothing_to_hide ft. ∆PSI KID Mercy{}"
+Programmer : ∆PSI KID Mercy{}
+Created on 5:54 PM, November 5 2018*/
+
+public class Program
+{
+    public static void main(String[] args) {
+       char a='a';
+       System.out.println("A Very basic program for printing English Alphabets");
+       for(char b=a;b<=(char)122;b++)
+       {
+       System.out.println("\t"+b);
+       }
+    }
+}
+/*This was a very helpful program for me today
+and played a huge role today to solve my problem so i decided to post it here*/
+
+// Thank you for your visit and your upvotes

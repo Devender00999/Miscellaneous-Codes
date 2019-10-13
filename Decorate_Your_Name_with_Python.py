42de4c422345b58f4cff8825947a38ff3434518c
PSI KID Mercy <30193989+PSI-KID-Mercy@users.noreply.github.com>
2019-10-13 17:19:50 +0530

    Create Decorate_Your_Name_with_Python.py
    In this Program, you enter two inputs 1. Your Name, 2. a Keyword to be used to form a rectangular 
    This Program decorates your input name with any type of keyword you entered, It decorates your name using a rectangular box which is formed using keyword entered

---
+++ Decorate_Your_Name_with_Python.py
@@ -0,0 +1,16 @@
+#Program:- Decorate your Good Name
+#Developers:- Mercy && Arya 
+#Please Enter your Good Name of 6 Characters & Decorater of 1 Character for best Results
+#Enter Your Name and Decorater in the first dialogbox but followed by an new line(enter)
+a = input("Please enter your Name: \n")
+
+print("\nYour Name Must be Decorated because We know you Deserve it ^_^\n")
+
+b = input("Please enter the Decorating Keyword: \n")
+
+print(b*38)
+print(b,b,"                              ",b,b)
+print(b,b,"        ",a,"is Great      "       ,b,b)
+print(b,b,"                              ",b,b)
+print(b*38)
+print("             ""Made by",a,", Mercy && Arya")

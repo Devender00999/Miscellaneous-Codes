50adaa51cb0d9eace05be25d6fbb668a792e3d62
japneetbhatia <56752427+japneetbhatia@users.noreply.github.com>
2020-10-05 13:28:51 +0530

    Create countvowel.py

---
+++ countvowel.py
@@ -0,0 +1,13 @@
+vowels = 'aeiou'
+
+str = 'Hello, Is this your first pull request?'
+
+str = str.casefold():
+
+count = {}.fromkeys(vowels,0)
+
+for char in str:
+   if char in count:
+       count[char] += 1
+
+print(count)
